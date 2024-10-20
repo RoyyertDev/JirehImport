9ef333e14d919a3d63476573bc18b2a9ef670bf2
RoyyertDev <royyertalfonso@gmail.com>
2024-10-20 12:06:58 -0400

    update offer.blade.php with new images of type product

--- resources/views/livewire/offer.blade.php
+++ resources/views/livewire/offer.blade.php
@@ -1,15 +1,37 @@
 <section class="pb-10">
     <h1 class="text-center font-semibold text-4xl pb-10 italic tracking-widest underline underline-offset-8 decoration-[#F2BB06]">Ofrecemos</h1>
     <div class="grid grid-cols-4 gap-6">
-        @for ($i = 0; $i < 4; $i++)
-            <article class="bg-[#0e0e0e] grid grid-rows-[250px_1fr] gap-4 p-4 rounded-lg shadow-[0_1px_8px_-5px_#F2BB06] hover:shadow-[0_3px_8px_-5px_#F2BB06] hover:scale-105 transition-all duration-300">
-                <figure class="bg-white w-full h-full rounded-lg">
-                    <img src="" alt="">
-                </figure>
-                <h1 class="text-center">Tipo de Producto {{ $i + 1 }}</h1>
-                <p class="text-zinc-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus.</p>
-                <button class="hover:text-[#F2BB06] w-auto m-auto border rounded-lg hover:border-[#F2BB06] transition-all duration-300 px-4 py-1">Ver más</button>
-            </article>
-        @endfor
+        <article class="bg-[#0e0e0e] grid grid-rows-[250px_1fr] gap-4 p-4 rounded-lg shadow-[0_1px_8px_-5px_#F2BB06] hover:shadow-[0_3px_8px_-5px_#F2BB06] hover:scale-105 transition-all duration-300">
+            <figure class="bg-white w-full h-full rounded-lg overflow-hidden">
+                <img src="./assets/img/ofrecemos/p1.png" alt="">
+            </figure>
+            <h1 class="text-center font-semibold text-xl">Exclusivo</h1>
+            <p class="text-zinc-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus.</p>
+            <button class="hover:text-[#F2BB06] w-auto m-auto border rounded-lg hover:border-[#F2BB06] transition-all duration-300 px-4 py-1">Ver más</button>
+        </article>
+        <article class="bg-[#0e0e0e] grid grid-rows-[250px_1fr] gap-4 p-4 rounded-lg shadow-[0_1px_8px_-5px_#F2BB06] hover:shadow-[0_3px_8px_-5px_#F2BB06] hover:scale-105 transition-all duration-300">
+            <figure class="bg-white w-full h-full rounded-lg overflow-hidden">
+                <img src="./assets/img/ofrecemos/p4.png" alt="">
+            </figure>
+            <h1 class="text-center font-semibold text-xl">Casual</h1>
+            <p class="text-zinc-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus.</p>
+            <button class="hover:text-[#F2BB06] w-auto m-auto border rounded-lg hover:border-[#F2BB06] transition-all duration-300 px-4 py-1">Ver más</button>
+        </article>
+        <article class="bg-[#0e0e0e] grid grid-rows-[250px_1fr] gap-4 p-4 rounded-lg shadow-[0_1px_8px_-5px_#F2BB06] hover:shadow-[0_3px_8px_-5px_#F2BB06] hover:scale-105 transition-all duration-300">
+            <figure class="bg-white w-full h-full rounded-lg overflow-hidden">
+                <img src="./assets/img/ofrecemos/p2.png" alt="">
+            </figure>
+            <h1 class="text-center font-semibold text-xl">Deportivo</h1>
+            <p class="text-zinc-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus.</p>
+            <button class="hover:text-[#F2BB06] w-auto m-auto border rounded-lg hover:border-[#F2BB06] transition-all duration-300 px-4 py-1">Ver más</button>
+        </article>
+        <article class="bg-[#0e0e0e] grid grid-rows-[250px_1fr] gap-4 p-4 rounded-lg shadow-[0_1px_8px_-5px_#F2BB06] hover:shadow-[0_3px_8px_-5px_#F2BB06] hover:scale-105 transition-all duration-300">
+            <figure class="bg-white w-full h-full rounded-lg overflow-hidden">
+                <img src="./assets/img/ofrecemos/p3.png" alt="">
+            </figure>
+            <h1 class="text-center font-semibold text-xl">Uniformes</h1>
+            <p class="text-zinc-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, voluptatibus.</p>
+            <button class="hover:text-[#F2BB06] w-auto m-auto border rounded-lg hover:border-[#F2BB06] transition-all duration-300 px-4 py-1">Ver más</button>
+        </article>
     </div>
 </section>

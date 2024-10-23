eb6c432a4a6f479d3ec645bdad6ab027f7728c28
RoyyertDev <royyertalfonso@gmail.com>
2024-10-23 13:03:47 +0000

    import swiper

--- resources/views/livewire/header-home.blade.php
+++ resources/views/livewire/header-home.blade.php
@@ -31,11 +31,13 @@
         <div class="swiper-button-next"></div>
         <div class="swiper-button-prev"></div>
     </div>
-    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
 </header>
 <!-- Carrusel para pantallas pequeñas -->
 <script type="module">
-    import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js';
+    // import Swiper JS
+        import Swiper from './swiper';
+        // import Swiper styles
+        import './swiper/css';
 
     document.addEventListener('DOMContentLoaded', () => {
         const swiper = new Swiper('.swiper-container', {

251eb86b87609acd9f489d627b295c7fe96b4b82
RoyyertDev <royyertalfonso@gmail.com>
2024-10-23 12:46:12 +0000

    install swiper

--- resources/views/livewire/header-home.blade.php
+++ resources/views/livewire/header-home.blade.php
@@ -1,4 +1,4 @@
-<header class="grid grid-cols-3 grid-rows-2 gap-5 max-h-[calc(100vh-120px)] mb-10">
+<header class="sm:grid grid-cols-3 grid-rows-2 gap-5 sm:max-h-[calc(100vh-120px)] mb-10">
     <figure class="col-span-2 row-span-2">
         <img src="./assets/img/header/Pasamontaña.jpeg"
             class="brightness-90 hover:brightness-125 hover:scale-[1.02] transition-all duration-300 object-fill w-full h-full rounded-lg shadow-[0_1px_8px_-5px_#F2BB06] hover:shadow-[0_4px_8px_-5px_#F2BB06]"
@@ -14,4 +14,40 @@
             class="brightness-90 hover:brightness-125 hover:scale-105 transition-all duration-300 object-fill w-full h-full rounded-lg shadow-[0_1px_8px_-5px_#F2BB06] hover:shadow-[0_4px_8px_-5px_#F2BB06]"
             alt="JirehImport Sueters">
     </figure>
+    <div class="swiper-container sm:hidden">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <img src="./assets/img/header/Pasamontaña.jpeg" alt="JirehImport Pasamontaña">
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/img/header/Sueters.jpeg" alt="JirehImport Sueters">
+            </div>
+            <div class="swiper-slide">
+                <img src="./assets/img/header/Sueters2.jpeg" alt="JirehImport Sueters">
+            </div>
+        </div>
+        <!-- Agrega paginación y navegación si es necesario -->
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+    </div>
 </header>
+<!-- Carrusel para pantallas pequeñas -->
+<script type="module">
+    import Swiper from 'swiper';
+    import 'swiper/swiper-bundle.css';
+
+    document.addEventListener('DOMContentLoaded', () => {
+        const swiper = new Swiper('.swiper-container', {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    });
+</script>
